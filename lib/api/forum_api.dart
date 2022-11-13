92639eb7594d3cde615eddc2f69f4fb645f41940
Yuen1010 <97172395+Yuen1010@users.noreply.github.com>
2022-11-13 16:41:25 +0800

    Update forum_api.dart

---
+++ lib/api/forum_api.dart
@@ -0,0 +1,33 @@
+Future<ThreadPage> getThreadPageData(
+    String cursor, int order, int pid, int fid, String q) async {
+  String Conditon = "?";
+  var response;
+
+  if (cursor != "") {
+    Conditon = Conditon + "cursor=" + cursor;
+  }
+  if (order != 0) {
+    Conditon = Conditon + "order=" + order.toString();
+  }
+  if (pid != 0) {
+    Conditon = Conditon + "pid=" + pid.toString();
+  }
+  if (fid != 0) {
+    Conditon = Conditon + "fid=" + fid.toString();
+  }
+  if (q != "") {
+    Conditon = Conditon + "q=" + q;
+  }
+
+  if (Conditon != "?") {
+    response = await http.get(Uri.parse(domain + "/thread" + Conditon));
+  } else {
+    response = await http.get(Uri.parse(domain + "/thread"));
+  }
+
+  if (response.statusCode == 200) {
+    return ThreadPage.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load ThreadPage');
+  }
+}
