dbe7bfbb39c2c629101d948045ebf29857926464
Yuen1010 <97172395+Yuen1010@users.noreply.github.com>
2022-11-13 16:51:21 +0800

    Update forum_api.dart

--- lib/api/forum_api.dart
+++ lib/api/forum_api.dart
@@ -1,28 +1,31 @@
  Future<ThreadPage> getThreadPageData(
     String cursor, int order, int pid, int fid, String q) async {
+  String domain = "";
+  // ignore: non_constant_identifier_names
   String Conditon = "?";
-  var response;
+  http.Response response;
 
-  if (cursor != "") {         //default String="",int=-1
-    Conditon = Conditon + "cursor=" + cursor;
+   //default String="",int=-1
+  if (cursor != "") {
+    Conditon = "${Conditon}cursor=$cursor";
   }
   if (order != -1) {
-    Conditon = Conditon + "order=" + order.toString();
+    Conditon = "${Conditon}order=$order";
   }
   if (pid != -1) {
-    Conditon = Conditon + "pid=" + pid.toString();
+    Conditon = "${Conditon}pid=$pid";
   }
   if (fid != -1) {
-    Conditon = Conditon + "fid=" + fid.toString();
+    Conditon = "${Conditon}fid=$fid";
   }
   if (q != "") {
-    Conditon = Conditon + "q=" + q;
+    Conditon = "${Conditon}q=$q";
   }
 
   if (Conditon != "?") {
-    response = await http.get(Uri.parse(domain + "/thread" + Conditon));
+    response = await http.get(Uri.parse("$domain/thread$Conditon"));
   } else {
-    response = await http.get(Uri.parse(domain + "/thread"));
+    response = await http.get(Uri.parse("$domain/thread"));
   }
 
   if (response.statusCode == 200) {
