6c401c5f5d76eac36e540f1f4bca6c9bee0c752d
Yuen1010 <97172395+Yuen1010@users.noreply.github.com>
2022-11-13 16:42:42 +0800

    Update forum_api.dart

--- lib/api/forum_api.dart
+++ lib/api/forum_api.dart
@@ -1,18 +1,18 @@
-Future<ThreadPage> getThreadPageData(
+ Future<ThreadPage> getThreadPageData(
     String cursor, int order, int pid, int fid, String q) async {
   String Conditon = "?";
   var response;
 
-  if (cursor != "") {
+  if (cursor != "") {         //default String="",int=-1
     Conditon = Conditon + "cursor=" + cursor;
   }
-  if (order != 0) {
+  if (order != -1) {
     Conditon = Conditon + "order=" + order.toString();
   }
-  if (pid != 0) {
+  if (pid != -1) {
     Conditon = Conditon + "pid=" + pid.toString();
   }
-  if (fid != 0) {
+  if (fid != -1) {
     Conditon = Conditon + "fid=" + fid.toString();
   }
   if (q != "") {
