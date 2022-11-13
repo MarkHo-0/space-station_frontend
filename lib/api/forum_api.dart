b762447ee27bb1bcba78db420e0fdf0630716fb0
Yuen1010 <97172395+Yuen1010@users.noreply.github.com>
2022-11-13 18:02:30 +0800

    Update forum_api.dart
    updated getThreadInside api

--- lib/api/forum_api.dart
+++ lib/api/forum_api.dart
@@ -2,17 +2,6 @@ import 'dart:convert';
 import 'package:flutter_application_3/class.dart';
 import 'package:http/http.dart' as http;
 
-
-Future<HomeData> getHomeData() async {
-  String domain = "";
-  var response = await http.get(Uri.parse("$domain/home"));
-  if (response.statusCode == 200) {
-    return HomeData.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load HomeData');
-  }
-}
-
 Future<ThreadPage> getThreadPageData(
     String cursor, int order, int pid, int fid, String q) async {
   String domain = "";
