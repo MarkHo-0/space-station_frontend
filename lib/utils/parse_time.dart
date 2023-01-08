a6b2960fb667b7da5b57445c1ea5520fe289fdd5
Mark Ho <s12059gsps@gmail.com>
2023-01-08 22:36:50 +0800

    Fix time parsing error on months and years

--- lib/utils/parse_time.dart
+++ lib/utils/parse_time.dart
@@ -7,8 +7,7 @@ const int oneMonth = 60 * 60 * 24 * 30;
 const int oneYear = 60 * 60 * 25 * 365;
 
 String unixTime2DiffText(int time) {
-  int now = (DateTime.now().millisecondsSinceEpoch / 1000).floor();
-  int diff = now - time;
+  int diff = getCurrUnixTime() - time;
 
   if (diff < 0) return 'time_future'.i18n();
   if (diff < 10) return 'time_just_now'.i18n();
@@ -16,9 +15,13 @@ String unixTime2DiffText(int time) {
   if (diff <= oneHour) return translateTime('time_minute', diff / oneMinute);
   if (diff <= oneDay) return translateTime('time_hour', diff / oneHour);
   if (diff <= oneMonth) return translateTime('time_day', diff / oneDay);
-  if (diff <= oneYear) return translateTime('time_year', diff / oneMonth);
+  if (diff <= oneYear) return translateTime('time_month', diff / oneMonth);
 
-  return translateTime('time_month', diff / oneYear);
+  return translateTime('time_year', diff / oneYear);
+}
+
+int getCurrUnixTime() {
+  return (DateTime.now().millisecondsSinceEpoch / 1000).floor();
 }
 
 String translateTime(String key, double num) {
