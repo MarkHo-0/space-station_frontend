11a9cf2285a75409ce05f3064e14e6dc411846ce
Yuen1010 <chanchiyuen9999@gmail.com>
2023-01-09 19:05:35 +0800

    Exception class

--- lib/api/error.dart
+++ lib/api/error.dart
@@ -1,4 +1,53 @@
 Exception handleException(int statusCode) {
-  //TODO: 錯誤代碼的處理
-  return Exception(statusCode);
+  switch (statusCode) {
+    case 400:
+      return GeneralError("General Error");
+    case 401:
+      return AuthorizationError("Authorization Error");
+    case 403:
+      return PermissionError("Permission Error");
+    case 404:
+      return NoResourceError("NoResource Error");
+    case 422:
+      return ParametersError("Parameters Error");
+    case 460:
+      return FrquentError("Frquent Error");
+    default:
+      return NetWorkError("NetWork Error");
+  }
+}
+
+class GeneralError implements Exception {
+  String errormsg;
+  GeneralError(this.errormsg);
+}
+
+class AuthorizationError implements Exception {
+  String errormsg;
+  AuthorizationError(this.errormsg);
+}
+
+class PermissionError implements Exception {
+  String errormsg;
+  PermissionError(this.errormsg);
+}
+
+class NoResourceError implements Exception {
+  String errormsg;
+  NoResourceError(this.errormsg);
+}
+
+class ParametersError implements Exception {
+  String errormsg;
+  ParametersError(this.errormsg);
+}
+
+class FrquentError implements Exception {
+  String errormsg;
+  FrquentError(this.errormsg);
+}
+
+class NetWorkError implements Exception {
+  String errormsg;
+  NetWorkError(this.errormsg);
 }
