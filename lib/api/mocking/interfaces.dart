b402c944731a03e4efcab8cf7fe6706d505a485d
Mark Ho <s12059gsps@gmail.com>
2023-01-20 22:48:52 +0800

    Add a new test API `/thread`

--- lib/api/mocking/interfaces.dart
+++ lib/api/mocking/interfaces.dart
@@ -1,52 +1,63 @@
-import 'client.dart';
-import 'fake_data.dart';
+import 'dart:convert';
+
 import '../../utils/parse_time.dart';
 
-List<dynamic> fakeThreads = [];
+import 'client.dart';
+import 'fake_data.dart';
+
+const _kThreadsPerFetch = 15;
+final _toBase64 = utf8.fuse(base64);
 
 void initializationInterfaces() {
   generateRandomThreads(50);
 
-  TestClient.onGet('/home', (_, __) {
+  TestClient.onGet('/home', (_) {
     Map<String, dynamic> body = {
-      'threads': getHotestThreads(5),
+      'threads': sortByTime(null, 5, 0),
       'news': getNews(),
+    };
+    return SimpleResponse(body);
+  });
+
+  TestClient.onGet('/thread', (req) {
+    final pid = int.parse(req.quaries['pid'] ?? '0');
+    final fid = int.parse(req.quaries['fid'] ?? '0');
+    final q = req.quaries['q'] ?? '';
+    final order = int.parse(req.quaries['order'] ?? '1');
+    final encodedCursor = req.quaries['cursor'] ?? '';
+
+    //過濾合適的貼文
+    final threads = filterThreads(pageID: pid, facultyID: fid, queryText: q);
+
+    int offset = 0;
+
+    //嘗試解析分頁指標
+    if (encodedCursor.isNotEmpty) {
+      final jsonCursor = _toBase64.decode(encodedCursor);
+      final mappedCursor = jsonDecode(jsonCursor);
+      offset = mappedCursor[1];
+    }
+
+    //排序
+    late List<dynamic> result;
+    if (order == 1) {
+      result = sortByTime(threads, _kThreadsPerFetch, offset);
+    } else {
+      result = sortByHotness(threads, _kThreadsPerFetch, offset);
+    }
+
+    //生成下一頁指標
+    String cursor = "";
+    if (result.length >= _kThreadsPerFetch) {
+      cursor = "[${getCurrUnixTime()},${offset + result.length}]";
+      cursor = _toBase64.encode(cursor);
+    }
+
+    Map<String, dynamic> body = {
+      'threads': result,
+      'continuous': cursor,
     };
 
     return SimpleResponse(body);
   });
-}
-
-List<dynamic> getHotestThreads(int count) {
-  final sorted = fakeThreads..sort((a, b) => (b['heat'] as int).compareTo(a['heat'] as int));
-  return sorted.take(count).toList();
-}
-
-void generateRandomThreads(int count) {
-  fakeThreads.clear();
-
-  for (var i = 1; i < count; i++) {
-    int pid = getRandomPID();
-    int fid = pid == 2 ? getRandomFID() : 0;
-    int lastUpdateTime = getRandomPassTime(getCurrUnixTime());
-    int createTime = getRandomPassTime(lastUpdateTime, percentInSameDay: 0.3);
-    Map<String, int> stats = getRandomThreadStats();
-    int heat = ((stats['like']! + stats['dislike']!) / 2).floor() + stats['comment']!;
-
-    dynamic thread = {
-      'tid': i,
-      'pid': pid,
-      'fid': fid,
-      'title': getRandomTitle(pid),
-      'sender': getRandomUser(),
-      'create_time': createTime,
-      'last_update_time': lastUpdateTime,
-      'stats': stats,
-      'content_cid': 1,
-      'pined_cid': getRandomThreadPinCid(),
-      'heat': heat,
-    };
-
-    fakeThreads.add(thread);
-  }
 }
