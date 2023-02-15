64b222caa525b419b04dad2f75fe1a8e9a4601d0
Yuen1010 <chanchiyuen9999@gmail.com>
2023-02-15 16:39:42 +0800

    forum_create page

--- lib/views/forum_pages/forum_create.dart
+++ lib/views/forum_pages/forum_create.dart
@@ -1,38 +1,100 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:space_station/models/thread.dart';
-import 'widgets/multi_tabs_thread_list.dart';
+import 'package:space_station/views/forum_pages/widgets/category_selector.dart';
+import 'package:space_station/views/forum_pages/widgets/multi_tabs_thread_list.dart';
 import './widgets/forum_top_panel.dart';
+import 'package:ez_localization/ez_localization.dart';
 import '../../api/interfaces/forum_api.dart';
 
-class ForumPostPage extends StatelessWidget {
+class ForumPostPage extends StatefulWidget {
   const ForumPostPage({super.key});
 
   @override
+  State<ForumPostPage> createState() => _ForumPostPageState();
+}
+
+class _ForumPostPageState extends State<ForumPostPage> {
+  int categoryID = 0;
+
+  @override
   Widget build(BuildContext context) {
-    const height = 200.0;
+    final titleController = TextEditingController();
+    final bodyController = TextEditingController();
 
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () => Navigator.pop(context),
+        appBar: AppBar(
+          title: Text(context.getString("create_post")),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.pop(context),
+          ),
+          actions: [Switch(value: true, onChanged: ((value) {}))],
         ),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 10),
-        child: SizedBox(
-          // <--- SizedBox
-          height: height,
-          child: TextField(
-            maxLines: height ~/ 10, // <--- maxLines
-            keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
-              hintText: 'Enter...',
+        body: Column(children: [
+          Container(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: CategorySelector(
+                    categoryKey: "faculty",
+                    quantity: 6,
+                    categoryID: categoryID,
+                    onChanged: ((categoryID) {
+                      this.categoryID = categoryID;
+                      setState(() {});
+                    }),
+                  ),
+                ),
+                Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: TextButton.icon(
+                      icon: Transform.rotate(
+                        angle: -3.1415926,
+                        child: const Icon(Icons.send),
+                      ),
+                      label: Text(context.getString("send_thread")),
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                      ),
+                    ))
+              ])),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: SizedBox(
+              // <--- SizedBox
+              child: TextField(
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: 'Title...',
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-    );
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            height: MediaQuery.of(context).size.height - 300,
+            child: Expanded(
+              child: TextField(
+                maxLines: null,
+                minLines: null,
+                expands: true,
+                textAlignVertical: TextAlignVertical.top,
+                controller: bodyController,
+                keyboardType: TextInputType.multiline,
+                decoration: const InputDecoration(
+                  hintText: 'Enter...',
+                ),
+              ),
+            ),
+          )
+        ]));
+  }
+
+  String regecx(TextEditingController body) {
+    Text(body.text);
+    return "";
   }
 }
